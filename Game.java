0c18210798887c3a4345c9fb4cda0100f995bbf1
Jessemal <31673958+Jessemal@users.noreply.github.com>
2017-11-30 19:03:36 -0600

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,16 @@
+package UltimateTicTacToe;
+
+public class Game {
+	
+	private enum State {X, O}
+	private final int SIZE = 3;
+	
+	private State[][] board = new State[3][3];
+	
+	
+	public boolean setBoard(int x, int y, State setVal) {
+		if (x > 
+	}
+}
+
+ 
